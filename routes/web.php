1040f50721d9e768048a57aac0057fcd094468c5
amlyhrzky <157791541+amlyhrzky@users.noreply.github.com>
2024-12-15 00:53:07 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/gaji/atur/', function () {
+    return view('/gaji/atur');
+});
+
+Route::get('/kasbon/lihat/', function () {
+    return view('/kasbon/lihat');
+});
