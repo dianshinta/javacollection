018de557f4fde8c59eb1c1107d82acde4b78c98b
Hana Hanifatul Hasanah <222212635@stis.ac.id>
2024-12-16 20:13:25 +0700

    perbaiki halaman beranda supervisor

--- resources/views/supervisor/beranda.blade.php
+++ resources/views/supervisor/beranda.blade.php
@@ -17,166 +17,117 @@
   <!-- CSS Files -->
   <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
   <link href="../assets/css/paper-dashboard.css?v=2.0.1" rel="stylesheet" />
+  <link href="../assets/css/supervisor-beranda.css" rel="stylesheet" />
 </head>
 
 <body class="">
   <div class="wrapper ">
     <div class="sidebar" data-color="white" data-active-color="danger">
-      <div class="logo">
-       
-        <span class="simple-text font-weight-bold">
-          JAVA COLLECTION
-        </span>
-      </div>
-
-      <!-- Menu -->
-      @include('supervisor.menu')
-      <!-- END MENU -->
+        <div class="logo">
+            <span class="simple-text font-weight-bold">
+            JAVA COLLECTION
+            </span>
+        </div>
+        <!-- Menu -->
+        @include('supervisor.menu')
+        <!-- END MENU -->
     </div>
 
-    {{-- content --}}
+    <!-- Content -->
     <div class="main-panel">
-      <div class="content">
-        <div class="mb-4">
-          <small class="text-muted d-block">Beranda</small>
-          <h5 class="font-weight-bold">Selamat datang!</h5>
+        <div class="content">
+            <div class="mb-4">
+            <small class="text-muted d-block">Beranda</small>
+            <h5 class="font-weight-bold">Selamat datang!</h5>
+            </div>
+            <!-- Nama, Hari, dan Lokasi -->
+            <div class="row ms-4">
+                <div class="d-flex justify-content-start align-items-center gap-4 mb-4">
+                    <div class="d-flex align-items-center mr-5">
+                        <i class="nc-icon nc-circle-10 text-primary mr-2"></i>          
+                        <div>
+                            <span>Putu Dian Shinta Prativi</span>
+                            <small class="text-muted d-block">222212822</small>
+                        </div>
+                    </div>
+                    <div class="d-flex align-items-center mr-5">
+                        <i class="nc-icon nc-calendar-60 text-primary mr-2"></i>
+                        <div>
+                        <span>Sabtu</span>
+                        <small class="text-muted d-block">26 Oktober 2024</small>
+                        </div>
+                    </div>  
+                    <div class="d-flex align-items-center me-4">
+                        <i class="nc-icon nc-pin-3 text-primary "></i>
+                        <div>
+                        <span>Cab. Tanah Abang</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <!-- Cabang -->
+            <div class="mt-4 mb-4 d-flex justify-content-end">
+                <select class="form-select w-auto" id="branch">
+                    <option>Cabang A</option>
+                    <option>Cabang B</option>
+                    <option>Cabang C</option>
+                </select>
+            </div>
+            <!-- Tabel -->
+            <div class="row">
+                <p class="font-weight-bold" style="margin-left: 1rem; margin-bottom: 0.5rem; font-size: 1.2rem;">Riwayat</p>
+                <div class="col-md-12">
+                    <div class="card ">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                            <table class="table">
+                                <thead class=" text-primary">
+                                <th>Nama</th>
+                                <th>Status</th>
+                                <th>Waktu</th>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Rafliansyah Dwi Setiawan Tondauu</td>
+                                        <td><span class="badge bg-success">Hadir</span></td>
+                                        <td>09.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>khaledd</td>
+                                        <td><span class="badge bg-secondary">none</span></td>
+                                        <td>none</td>
+                                    </tr>
+                                    <tr>
+                                        <td>khaledd</td>
+                                        <td><span class="badge bg-warning text-dark">terlambat</span></td>
+                                        <td>11.00</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="d-flex justify-content-start align-items-center gap-4 mb-4">
-          <div class="d-flex align-items-center mr-5">
-            <i class="nc-icon nc-circle-10 text-primary mr-2"></i>          
-            <div>
-              <span>Putu Dian Shinta Prativi</span>
-              <small class="text-muted d-block">222212822</small>
+
+        <!-- Footer -->
+        <footer class="footer footer-black  footer-white ">
+            <div class="container-fluid">
+            <div class="row">
+                <nav class="footer-nav">
+                <ul>
+                    <li><a href="https://www.creative-tim.com" target="_blank">Creative Tim</a></li>
+                    <li><a href="https://www.creative-tim.com/blog" target="_blank">Blog</a></li>
+                    <li><a href="https://www.creative-tim.com/license" target="_blank">Licenses</a></li>
+                </ul>
+                </nav>
             </div>
-          </div>
-          
-          <div class="d-flex align-items-center mr-5">
-            <i class="nc-icon nc-calendar-60 text-primary mr-2"></i>
-            <div>
-              <span>Sabtu</span>
-              <small class="text-muted d-block">26 Oktober 2024</small>
             </div>
-          </div>
-          
-          <div class="d-flex align-items-center me-4">
-            <i class="nc-icon nc-pin-3 text-primary mr-2"></i>
-            <div>
-              <span>Cab. Tanah Abang</span>
-            </div>
-          </div>
-        </div>
-        <hr>
-        <div class="row">
-          <div class="col-lg-3 col-md-6 col-sm-6">
-            <div class="card card-stats">
-              <div class="card-body ">
-                <div class="row">
-                  <div class="col-12 d-flex justify-content-start align-items-center">
-                    <span id="time-display" class="mr-3 font-weight-bold" style="font-size:1.5rem;">07.10</span>
-                    <button class="btn btn-success" style="font-size: 1rem; color: black;">Hadir</button>
-                  </div>
-        
-                </div>
-              </div>
-              <div class="card-footer ">
-                <hr>
-                <div class="stats">
-                  <i class="fa fa-clock-o"></i>
-                  In the last hour
-                </div>
-              </div>
-            </div>
-          </div>
-          
-        </div>
-        <div class="row">
-          <div class="col-md-12">
-            <div class="card ">
-              <div class="card-header ">
-                <h5 class="card-title">Users Behavior</h5>
-                <p class="card-category">24 Hours performance</p>
-              </div>
-              <div class="card-body ">
-                <canvas id=chartHours width="400" height="100"></canvas>
-              </div>
-              <div class="card-footer ">
-                <hr>
-                <div class="stats">
-                  <i class="fa fa-history"></i> Updated 3 minutes ago
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-4">
-            <div class="card ">
-              <div class="card-header ">
-                <h5 class="card-title">Email Statistics</h5>
-                <p class="card-category">Last Campaign Performance</p>
-              </div>
-              <div class="card-body ">
-                <canvas id="chartEmail"></canvas>
-              </div>
-              <div class="card-footer ">
-                <div class="legend">
-                  <i class="fa fa-circle text-primary"></i> Opened
-                  <i class="fa fa-circle text-warning"></i> Read
-                  <i class="fa fa-circle text-danger"></i> Deleted
-                  <i class="fa fa-circle text-gray"></i> Unopened
-                </div>
-                <hr>
-                <div class="stats">
-                  <i class="fa fa-calendar"></i> Number of emails sent
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-8">
-            <div class="card card-chart">
-              <div class="card-header">
-                <h5 class="card-title">NASDAQ: AAPL</h5>
-                <p class="card-category">Line Chart with Points</p>
-              </div>
-              <div class="card-body">
-                <canvas id="speedChart" width="400" height="100"></canvas>
-              </div>
-              <div class="card-footer">
-                <div class="chart-legend">
-                  <i class="fa fa-circle text-info"></i> Tesla Model S
-                  <i class="fa fa-circle text-warning"></i> BMW 5 Series
-                </div>
-                <hr />
-                <div class="card-stats">
-                  <i class="fa fa-check"></i> Data information certified
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <footer class="footer footer-black  footer-white ">
-        <div class="container-fluid">
-          <div class="row">
-            <nav class="footer-nav">
-              <ul>
-                <li><a href="https://www.creative-tim.com" target="_blank">Creative Tim</a></li>
-                <li><a href="https://www.creative-tim.com/blog" target="_blank">Blog</a></li>
-                <li><a href="https://www.creative-tim.com/license" target="_blank">Licenses</a></li>
-              </ul>
-            </nav>
-            <div class="credits ml-auto">
-              <span class="copyright">
-                © <script>
-                  document.write(new Date().getFullYear())
-                </script>, made with <i class="fa fa-heart heart"></i> by Creative Tim
-              </span>
-            </div>
-          </div>
-        </div>
-      </footer>
+        </footer>
     </div>
-    {{-- end content --}}
+    <!-- End Content -->
 
   </div>
   <!--   Core JS Files   -->
