49e42c39b1e74b33722ad5ca58a6e276b955bb8d
Putu Dian Shinta Prativi <222212822@stis.ac.id>
2024-12-18 13:25:45 +0700

    making presensi model

---
+++ app/Models/presensi.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class presensi extends Model
+{
+    protected $table = 'kehadiran';
+    public $timestamps = false;
+    protected $primaryKey = 'id'; // Kolom primary key (id)
+    public $incrementing = true; // Pastikan auto-increment diaktifkan
+    protected $keyType = 'int'; // Pastikan tipe data id adalah integer    
+    protected $fillable = ['status', 'tanggal', 'waktu', 'toko', 'nip'];
+
+    // use HasFactory;
+}
