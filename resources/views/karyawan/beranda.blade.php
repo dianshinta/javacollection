1eaa311fd0706bd11268c0f92ecfc7cbf9fb6d31
Laure <laurentiustrisna@gmail.com>
2024-12-18 11:23:41 +0700

    selesai dashboard

--- resources/views/karyawan/beranda.blade.php
+++ resources/views/karyawan/beranda.blade.php
@@ -21,7 +21,7 @@ Coded by www.creative-tim.com
   <link rel="icon" type="image/png" href="../assets/img/favicon.png">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
-    Beranda
+    Paper Dashboard 2 by Creative Tim
   </title>
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
   <!--     Fonts and icons     -->
@@ -30,7 +30,9 @@ Coded by www.creative-tim.com
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
   <!-- CSS Files -->
   <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="../assets/css/dashboard.css">
   <link href="../assets/css/paper-dashboard.css?v=2.0.1" rel="stylesheet" />
+  <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
   <!-- CSS Just for demo purpose, don't include it in your project -->
   <!-- <link href="../assets/demo/demo.css" rel="stylesheet" /> -->
 </head>
@@ -47,13 +49,13 @@ Coded by www.creative-tim.com
       <div class="sidebar-wrapper">
         <ul class="nav">
           <li class="active ">
-            <a href="{{route('beranda')}}">
+            <a href="./dashboard.html">
               <i class="nc-icon nc-layout-11"></i>
               <p>Beranda</p>
             </a>
           </li>
           <li>
-            <a href="{{route('presensi')}}">
+            <a href="./icons.html">
               <i class="nc-icon nc-touch-id"></i>
               <p>Presensi</p>
             </a>
@@ -66,12 +68,12 @@ Coded by www.creative-tim.com
             <div class="collapse" id="pengajuanDropdown">
               <ul class="nav" style="margin-left: 62px;">
                 <li>
-                  <a href="{{route('perizinan')}}" ">
+                  <a href="./perizinan.html" ">
                     <p>Perizinan</p>
                   </a>
                 </li>
                 <li>
-                  <a href="{{route(name: 'kasbon')}}">
+                  <a href="./pengajuan2.html">
                     <p>Kasbon</p>
                   </a>
                 </li>
@@ -124,7 +126,6 @@ Coded by www.creative-tim.com
             </div>
           </div>
         </div>
-        <hr>
         <div class="row">
           <div class="col-lg-3 col-md-6 col-sm-6">
             <div class="card card-stats">
@@ -134,7 +135,6 @@ Coded by www.creative-tim.com
                     <span id="time-display" class="mr-3 font-weight-bold" style="font-size:1.5rem;">07.10</span>
                     <button class="btn btn-success" style="font-size: 1rem; color: black;">Hadir</button>
                   </div>
-        
                 </div>
               </div>
               <div class="card-footer ">
@@ -146,68 +146,35 @@ Coded by www.creative-tim.com
               </div>
             </div>
           </div>
-          
         </div>
+        
         <div class="row">
-          <div class="col-md-12">
-            <div class="card ">
-              <div class="card-header ">
-                <h5 class="card-title">Users Behavior</h5>
-                <p class="card-category">24 Hours performance</p>
-              </div>
-              <div class="card-body ">
-                <canvas id=chartHours width="400" height="100"></canvas>
-              </div>
-              <div class="card-footer ">
-                <hr>
-                <div class="stats">
-                  <i class="fa fa-history"></i> Updated 3 minutes ago
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-4">
-            <div class="card ">
-              <div class="card-header ">
-                <h5 class="card-title">Email Statistics</h5>
-                <p class="card-category">Last Campaign Performance</p>
-              </div>
-              <div class="card-body ">
-                <canvas id="chartEmail"></canvas>
-              </div>
-              <div class="card-footer ">
-                <div class="legend">
-                  <i class="fa fa-circle text-primary"></i> Opened
-                  <i class="fa fa-circle text-warning"></i> Read
-                  <i class="fa fa-circle text-danger"></i> Deleted
-                  <i class="fa fa-circle text-gray"></i> Unopened
-                </div>
-                <hr>
-                <div class="stats">
-                  <i class="fa fa-calendar"></i> Number of emails sent
-                </div>
-              </div>
-            </div>
-          </div>
           <div class="col-md-8">
             <div class="card card-chart">
               <div class="card-header">
-                <h5 class="card-title">NASDAQ: AAPL</h5>
-                <p class="card-category">Line Chart with Points</p>
+                <h5 class="card-title">
+                    Kehadiran<br>
+                    Karyawan
+                </h5>
+                <p class="card-category">Bulanan</p>
               </div>
               <div class="card-body">
-                <canvas id="speedChart" width="400" height="100"></canvas>
+                <canvas id="barChart"></canvas>
               </div>
-              <div class="card-footer">
-                <div class="chart-legend">
-                  <i class="fa fa-circle text-info"></i> Tesla Model S
-                  <i class="fa fa-circle text-warning"></i> BMW 5 Series
-                </div>
-                <hr />
-                <div class="card-stats">
-                  <i class="fa fa-check"></i> Data information certified
+              <div class="card-footer ">
+                <div class="legend">
+                  <div class="legend-list">
+                    <i class="fa fa-circle" style="color: rgba(0, 183, 255, 0.8);"></i>Hadir
+                  </div>
+                  <div class="legend-list">
+                    <i class="fa fa-circle" style="color: rgba(139, 69, 19, 0.8);"></i>Izin
+                  </div>
+                  <div class="legend-list">
+                    <i class="fa fa-circle" style="color: rgba(255, 255, 0, 0.8);"></i>Terlambat
+                  </div>
+                  <div class="legend-list">
+                    <i class="fa fa-circle" style="color: rgba(255, 0, 0, 0.8);"></i>Tanpa Keterangan
+                  </div>
                 </div>
               </div>
             </div>
@@ -244,12 +211,13 @@ Coded by www.creative-tim.com
   <!--  Google Maps Plugin    -->
   <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
   <!-- Chart JS -->
-  <script src="../assets/js/plugins/chartjs.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.0"></script>
   <!--  Notifications Plugin    -->
   <script src="../assets/js/plugins/bootstrap-notify.js"></script>
   <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
   <script src="../assets/js/paper-dashboard.min.js?v=2.0.1" type="text/javascript"></script><!-- Paper Dashboard DEMO methods, don't include it in your project! -->
-  <script src="../assets/demo/demo.js"></script>
+  <script src="../assets/demo/demo-employee.js"></script>
+
   <script>
     $(document).ready(function() {
       // Javascript method's body can be found in assets/assets-for-demo/js/demo.js
