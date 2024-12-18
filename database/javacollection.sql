ea1265cbe22ea7d7520a9f981be06fb13cc2528c
Putu Dian Shinta Prativi <222212822@stis.ac.id>
2024-12-18 13:28:11 +0700

    just storing the database we are using

---
+++ database/javacollection.sql
@@ -0,0 +1,177 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Dec 16, 2024 at 09:59 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `javacollection`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `karyawan`
+--
+
+CREATE TABLE `karyawan` (
+  `nip` varchar(20) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `tempat_lahir` varchar(50) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `gender` enum('L','P') NOT NULL,
+  `toko_id` int(11) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_telp` varchar(15) NOT NULL,
+  `bank` varchar(50) NOT NULL,
+  `no_rek` varchar(20) NOT NULL,
+  `kasbon` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kasbon`
+--
+
+CREATE TABLE `kasbon` (
+  `id` int(11) NOT NULL,
+  `status` enum('Lunas','Belum Lunas') NOT NULL,
+  `tanggal_pengajuan` date NOT NULL,
+  `alasan` text NOT NULL,
+  `nominal_diajukan` int(11) NOT NULL,
+  `nominal_dibayar` int(11) NOT NULL,
+  `nip` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kehadiran`
+--
+
+CREATE TABLE `kehadiran` (
+  `id` int(11) NOT NULL,
+  `status` enum('Hadir','Terlambat','None') NOT NULL,
+  `tanggal` date NOT NULL,
+  `waktu` time NOT NULL,
+  `toko` varchar(50) NOT NULL,
+  `nip` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manajer`
+--
+
+CREATE TABLE `manajer` (
+  `nip` varchar(20) NOT NULL,
+  `nama` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `perizinan`
+--
+
+CREATE TABLE `perizinan` (
+  `id` int(11) NOT NULL,
+  `lampiran` text NOT NULL,
+  `tanggal` date NOT NULL,
+  `jenis` enum('Sakit','Cuti','Lainnya') NOT NULL,
+  `keterangan` text NOT NULL,
+  `status` enum('Diproses','Disetujui','Ditolak') NOT NULL,
+  `nip` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `slipgaji`
+--
+
+CREATE TABLE `slipgaji` (
+  `id` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `jml_hadir` int(11) NOT NULL,
+  `kasbon` int(11) NOT NULL,
+  `bonus` int(11) NOT NULL,
+  `tunjangan` int(11) NOT NULL,
+  `potongan` int(11) NOT NULL,
+  `gaji_net` int(11) NOT NULL,
+  `slip_gaji` varchar(255) NOT NULL,
+  `nip` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `toko`
+--
+
+CREATE TABLE `toko` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `penjualan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `nip` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `jabatan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `karyawan`
+--
+ALTER TABLE `karyawan`
+  ADD PRIMARY KEY (`nip`);
+
+--
+-- Indexes for table `manajer`
+--
+ALTER TABLE `manajer`
+  ADD PRIMARY KEY (`nip`);
+
+--
+-- Indexes for table `perizinan`
+--
+ALTER TABLE `perizinan`
+  ADD PRIMARY KEY (`nip`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`nip`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
