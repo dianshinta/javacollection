e49f04970b1046c9c7e7e51180b39380b3c47890
Putu Dian Shinta Prativi <222212822@stis.ac.id>
2024-12-18 13:22:09 +0700

    adjustment for the database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->string('nip')->id();
+            // $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp(column: 'email_verified_at')->nullable();
             $table->string('password');
+            $table->string('jabatan');
             $table->rememberToken();
             $table->timestamps();
         });
