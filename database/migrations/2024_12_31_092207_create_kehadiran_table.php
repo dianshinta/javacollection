d8db3d39ce4f27464bd3893fe4cf439bd95b90fc
Putu Dian Shinta Prativi <222212822@stis.ac.id>
2024-12-31 12:07:05 +0700

    fixing create_kehadiran_table migration file

---
+++ database/migrations/2024_12_31_092207_create_kehadiran_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKehadiranTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kehadiran', function (Blueprint $table) {
+            $table->id();
+            $table->enum('status', ['Hadir', 'Terlambat', 'Tidak Hadir']);
+            $table->date('tanggal');
+            $table->time('waktu');
+            $table->string('toko', 50);
+            $table->string('nip', 20);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kehadiran');
+    }
+}
