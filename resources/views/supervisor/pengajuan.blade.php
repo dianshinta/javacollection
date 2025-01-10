6391c326fde2beeab860bdee4084e8aaf132b435
Hana Hanifatul Hasanah <222212635@stis.ac.id>
2025-01-10 11:37:14 +0700

    delete footer on the supervisor page

--- resources/views/supervisor/pengajuan.blade.php
+++ resources/views/supervisor/pengajuan.blade.php
@@ -145,20 +145,6 @@
                 </div>
             </div>
 
-            <!-- Footer -->
-            <footer class="footer footer-black  footer-white ">
-                <div class="container-fluid">
-                <div class="row">
-                    <nav class="footer-nav">
-                    <ul>
-                        <li><a href="https://www.creative-tim.com" target="_blank">Creative Tim</a></li>
-                        <li><a href="https://www.creative-tim.com/blog" target="_blank">Blog</a></li>
-                        <li><a href="https://www.creative-tim.com/license" target="_blank">Licenses</a></li>
-                    </ul>
-                    </nav>
-                </div>
-                </div>
-            </footer>
         </div>
     </div>
 
